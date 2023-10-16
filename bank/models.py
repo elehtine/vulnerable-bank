bf43d97251c2c9d8704aaac412d961ace6ea6b44
Elias Lehtinen <elias.lehtinen01@gmail.com>
2023-10-16 09:32:08 +0300

    Create account model

--- bank/models.py
+++ bank/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    iban = models.CharField(max_length=30)
+    balance = models.IntegerField()
