0fe36a03fab2c815cb0de540fa51ae4fbc035236
Elias Lehtinen <elias.lehtinen01@gmail.com>
2023-10-17 12:17:06 +0300

    Url and view for account page

--- bank/views.py
+++ bank/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello World!")
+
+
+def account(request, iban):
+    return HttpResponse(f"Account: {iban}")
