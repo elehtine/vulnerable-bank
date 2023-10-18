a3408cc97d9155b464dea931b974eaa10bdb3db0
Elias Lehtinen <elias.lehtinen01@gmail.com>
2023-10-18 13:54:54 +0300

    Initialise login template

--- bank/views.py
+++ bank/views.py
@@ -1,9 +1,12 @@
+from django.conf import settings
 from django.shortcuts import HttpResponse, redirect
 
 from .models import Account
 
 
 def index(request):
+    if not request.user.is_authenticated:
+        return redirect(f"{settings.LOGIN_URL}?next={request.path}")
     return HttpResponse("Hello World!")
 
 
--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
 
 urlpatterns = [
-    path('bank/', include('bank.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/login/', LoginView.as_view(template_name="bank/login.html")),
+    path('bank/', include('bank.urls')),
 ]
