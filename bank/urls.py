a8e27275d08e8b22e608e0b99520407b4605ffdc
Elias Lehtinen <elias.lehtinen01@gmail.com>
2023-10-19 12:28:40 +0300

    Change double to single quote. Template for bank main page

--- bank/urls.py
+++ bank/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-app_name = "bank"
+app_name = 'bank'
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<iban>/", views.account, name="account"),
+    path('', views.index, name='index'),
+    path('<iban>/', views.account, name='account'),
 ]
--- bank/views.py
+++ bank/views.py
@@ -1,19 +1,19 @@
 from django.conf import settings
-from django.shortcuts import HttpResponse, redirect
+from django.shortcuts import HttpResponse, redirect, render
 
 from .models import Account
 
 
 def index(request):
     if not request.user.is_authenticated:
-        return redirect(f"{settings.LOGIN_URL}?next={request.path}")
-    return HttpResponse("Hello World!")
+        return redirect(f'{settings.LOGIN_URL}?next={request.path}')
+    return render(request, 'bank/index.html')
 
 
 def account(request, iban):
     try:
         account = Account.objects.get(iban=iban)
     except:
-        return redirect("bank:index")
+        return redirect('bank:index')
 
-    return HttpResponse(f"{iban}: {account.balance}")
+    return HttpResponse(f'{iban}: {account.balance}')
