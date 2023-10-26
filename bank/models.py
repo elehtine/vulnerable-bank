85f96a716019e7338269f915be11d3568ab7a426
Elias Lehtinen <elias.lehtinen01@gmail.com>
2023-10-26 09:59:15 +0300

    Add user for accounts

--- bank/models.py
+++ bank/models.py
@@ -1,7 +1,13 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.db import models
 
 
 class Account(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        default=User.objects.get(username="admin").pk,
+    )
     iban = models.CharField(max_length=30, unique=True)
     balance = models.IntegerField()
